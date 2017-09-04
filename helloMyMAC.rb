cbd049769598de6fc33f6f4965324cbd10194f5f
Dmitri <dimi615@pisem.net>
2017-09-04 21:52:49 +0300

    helloMyMAC.rb Double quotes

--- helloMyMAC.rb
+++ helloMyMAC.rb
@@ -1,16 +1,14 @@
-require 'formula'
-
 class Hellomymac < Formula
-  desc 'This is Hello World BASH script for Mac OS'
-  homepage 'http://dimig.blogspot.com'
-  url 'https://github.com/DimiG/myBASHscripts/raw/master/bin/helloMyMAC-1.0.0.tar.gz'
-  sha256 'a35e153b547fe79b5e1addec8fcf84b69e8fdf9bc3d11694b5f2a30a52bb88b7'
+  desc "This is Hello World BASH script for Mac OS"
+  homepage "http://dimig.blogspot.com"
+  url "https://raw.githubusercontent.com/DimiG/myBASHscripts/master/bin/helloMyMAC-1.0.0.tar.gz"
+  sha256 "a35e153b547fe79b5e1addec8fcf84b69e8fdf9bc3d11694b5f2a30a52bb88b7"
   version "1.0.0"
 
   bottle :unneeded
 
   def install
-    bin.install 'helloMyMAC.sh'
+    bin.install "helloMyMAC.sh"
   end
 end
 
