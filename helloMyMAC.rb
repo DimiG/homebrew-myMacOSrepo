43d7e10d37b56bb18ab7e694a73e57965039bf23
Dmitri <dimi615@pisem.net>
2017-09-04 20:44:35 +0300

    helloMyMAC.rb SHA256 should working now...

--- helloMyMAC.rb
+++ helloMyMAC.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Hellomymac < Formula
   desc 'This is Hello World BASH script for Mac OS'
   homepage 'http://dimig.blogspot.com'
-  url 'https://github.com/DimiG/myBASHscripts/blob/master/bin/helloMyMAC-1.0.0.tar.gz'
-  sha256 '5fe60c16464886ca7b5d95fb475282e2ec7acc69bc97208d75466c902be885f8'
+  url 'https://github.com/DimiG/myBASHscripts/raw/master/bin/helloMyMAC-1.0.0.tar.gz'
+  sha256 'a35e153b547fe79b5e1addec8fcf84b69e8fdf9bc3d11694b5f2a30a52bb88b7'
   version "1.0.0"
 
   bottle :unneeded
